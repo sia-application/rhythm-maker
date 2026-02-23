a66d9c51809ecf69c89dd397e97eed910770cb86
m81-ito-keisuke <larc_bump717kei@yahoo.co.jp>
2026-02-23 15:11:32 +0900

    sw

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'sw-v32';
+const CACHE_NAME = 'sw-v33';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
-    './style.css?v=1.6.1',
-    './script.js?v=1.6.1',
+    './style.css?v=1.4.4',
+    './script.js?v=1.4.4',
     './manifest.json',
     './icon-192.png',
     './icon-512.png'
